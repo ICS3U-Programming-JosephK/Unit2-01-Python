29a456113b3db29f3f860aca60bc2da7df0a39f8
JosephK2006 <jiyong.kwon@stu.ocsb.ca>
2022-09-22 03:46:43 +0000

    added area_per_circle.py

---
+++ area_per_circle.py
@@ -0,0 +1,12 @@
+import math
+
+#Variable to define radius 
+rad = 15
+circ = math.pi*2*rad
+area = math.pi*rad**2
+print("If a cricle has a raidus of 15mm:")
+print("")
+print("Area is {}mm^2".format(area))
+print("The circumference is {}mm".format(circ))
+
+
